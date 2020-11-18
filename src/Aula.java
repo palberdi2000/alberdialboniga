382b51247e608d4655172924091f43301cf108f3
Julen Alboni <julen.albonigamenor@opendeusto.es>
2020-11-18 14:32:15 +0100

    correccion metodos

--- src/Aula.java
+++ src/Aula.java
@@ -2,20 +2,14 @@
 public class Aula {
 	private int numAula;
 	private int capacidad;
-	private Alumno[]lista_alumnos;
-	private Profesor profe;
-	private String asignatura;
 	
 	public Aula() {
-		this.lista_alumnos= new Alumno[capacidad] ;
+		
 	}
 	
-	public Aula(int numAula,int capacidad, Alumno lista_alumnos, Profesor profe, String asignatura) {
+	public Aula(int numAula,int capacidad) {
 		this.capacidad=capacidad;
 		this.numAula=numAula;
-		this.lista_alumnos= new Alumno[capacidad] ;
-		this.profe=profe;
-		this.asignatura=asignatura;
 	}
 
 	public int getNumAula() {
@@ -33,76 +27,13 @@ public class Aula {
 	public void setCapacidad(int capacidad) {
 		this.capacidad = capacidad;
 	}
-
-	public Alumno[] getLista_alumnos() {
-		return lista_alumnos;
-	}
-
-	public void setLista_alumnos(Alumno[] lista_alumnos) {
-		this.lista_alumnos = lista_alumnos;
-	}
-
-	public Profesor getProfe() {
-		return profe;
-	}
-
-	public void setProfe(Profesor profe) {
-		this.profe = profe;
-	}
-
-	public String getAsignatura() {
-		return asignatura;
-	}
-
-	public void setAsignatura(String asignatura) {
-		this.asignatura = asignatura;
-	}
-
-	public void anyadirAlumno(Alumno a)
+		
+	public void capacidadClase() throws Exception
 	{
-		for(int i=0;i<capacidad;i++)
+		if(capacidad>=15)
 		{
-			if(lista_alumnos[i]==null)
-			{
-				lista_alumnos[i] = a;
-			}
+			throw new Exception();
 		}
-	}
-	
-	public void recomendacion()
-	{
-		int contador=0;
-		for(int i=0;i<capacidad;i++)
-		{
-			if(lista_alumnos[i].getNota()<5)
-			{
-				contador++;
-				System.out.println("Se le recomienda a " + lista_alumnos[i].getNombre() + lista_alumnos[i].getApellido() + "que se apunte a las clases para preparar la recuperacion de " + lista_alumnos[i].getAsigMatriculada());
-			}
-		}
-	}
-	
-	public boolean clase()
-	{
-		if(this.asignatura != this.profe.getAsigImpartida())
-		{
-			System.out.println("El profesor " + this.profe.getNombre() + this.profe.getApellido() + "se ha equivocado de aula");
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	}
-	
-	public void capacidadClase(Alumno[]lista_alumnos) throws Exception
-	{
-		for(Alumno a: lista_alumnos)
-		{
-			if(capacidad>=15)
-			{
-				throw new Exception();
-			}
-		}
+		
 	}
 }
--- src/Grupo.java
+++ src/Grupo.java
@@ -1,13 +1,23 @@
+import java.util.ArrayList;
 
 public class Grupo {
 	private int numGrupo;
+	private Alumno alumno;
+	private Aula aula;
+	private ArrayList<Alumno>lista_alumnos;
+	private Profesor profe;
 	
 	public Grupo() {
-		
+		this.lista_alumnos=new ArrayList<Alumno>();
 	}
-	
-	public Grupo(int numGrupo) {
-		this.numGrupo=numGrupo;
+
+	public Grupo(int numGrupo, Alumno alumno, Aula aula, ArrayList<Alumno> lista_alumnos, Profesor profe) {
+		super();
+		this.numGrupo = numGrupo;
+		this.alumno = alumno;
+		this.aula = aula;
+		this.lista_alumnos = lista_alumnos;
+		this.profe=profe;
 	}
 
 	public int getNumGrupo() {
@@ -18,6 +28,47 @@ public class Grupo {
 		this.numGrupo = numGrupo;
 	}
 
+	public Alumno getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+
+	public Aula getAula() {
+		return aula;
+	}
+
+	public void setAula(Aula aula) {
+		this.aula = aula;
+	}
 	
+	public ArrayList<Alumno> getLista_alumnos() {
+		return lista_alumnos;
+	}
+
+	public void setLista_alumnos(ArrayList<Alumno> lista_alumnos) {
+		this.lista_alumnos = lista_alumnos;
+	}
+
+	public Profesor getProfe() {
+		return profe;
+	}
+
+	public void setProfe(Profesor profe) {
+		this.profe = profe;
+	}
+
+	public void anyadirAlumno(Alumno a)
+	{
+		{
+			lista_alumnos.add(a);
+		}
+	}
 	
+	public void anyadirProfesor(Profesor p)
+	{
+		
+	}
 }
