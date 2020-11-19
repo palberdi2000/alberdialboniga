4e5d197771e2ad3ddc064b19e72403e940757d5b
Julen Alboni <julen.albonigamenor@opendeusto.es>
2020-11-19 18:28:49 +0100

    create tables

--- src/GestorBD.java
+++ src/GestorBD.java
@@ -22,17 +22,17 @@ public class GestorBD {
         try (Connection conn = DriverManager.getConnection(url);
              Statement stmt = conn.createStatement()) {
 
-            String cr1 = "CREATE TABLE ALUMNO";
+            String cr1 = "CREATE TABLE ALUMNO(ID_ALUMNO NUMBER NOT NULL PRIMARY KEY, NOMBRE VARCHAR2(25) NOT NULL, APELLIDO VARCHAR2(25) NOT NULL, CURSO VARCHAR2(25) NOT NULL)";
+      	
+            String cr2 = "CREATE TABLE PROFESOR(ID_PROFESOR NUMBER NOT NULL PRIMARY KEY, NOMBRE VARCHAR2(25) NOT NULL, APELLIDO VARCHAR2(25) NOT NULL)";
 
-            String cr2 =
+            String cr3 = "CREATE TABLE AULA(ID_AULA NUMBER NOT NULL PRIMARY KEY, NOMBRE VARCHAR2(25) NOT NULL, CAPACIDAD NUMBER NOT NULL)";
 
-            String cr3 = 
+            String cr4 = "CREATE TABLE ASIGNATURA(ID_ASIGNATURA NUMBER NOT NULL PRIMARY KEY, NOMBRE VARCHAR(25) NOT NULL)";
 
-            String cr4 = 
-
-            String cr5 = 
+            String cr5 = "CREATE TABLE ALUMNO_GRUPO(ID_ALUMNO NUMBER NOT NULL, ID_GRUPO NUMBER NOT NULL)";
             
-            String sql6 = 
+            String CR6 = "CREATE TABLE GRUPO(ID_GRUPO NUMBER NOT NULL PRIMARY KEY, ID_ALUMNO NUMBER NOT NULL, ID_PROFESOR NUMBER NOT NULL, ID_AULA NUMBER NOT NULL, ID_ASIGNATURA NUMBER NOT NULL)";   
             
          //--------------------------------------------------------
             
