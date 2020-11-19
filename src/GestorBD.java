afe7db70cde082221c8cce3fba31dffd2358a492
Julen Alboni <julen.albonigamenor@opendeusto.es>
2020-11-19 18:33:38 +0100

    create tables II

--- src/GestorBD.java
+++ src/GestorBD.java
@@ -34,29 +34,7 @@ public class GestorBD {
             
             String CR6 = "CREATE TABLE GRUPO(ID_GRUPO NUMBER NOT NULL PRIMARY KEY, ID_ALUMNO NUMBER NOT NULL, ID_PROFESOR NUMBER NOT NULL, ID_AULA NUMBER NOT NULL, ID_ASIGNATURA NUMBER NOT NULL)";   
             
-         //--------------------------------------------------------
-            
-            String sql7 = "CREATE TABLE IF NOT EXISTS RECEIPT (\n"
-                    + "    idReceipt integer NOT NULL,\n"
-                    + "    idCommand integer NOT NULL,\n"
-                    + "    hour DATETIME NOT NULL,\n"
-                    + "    price real,\n"
-                    + "    FOREIGN KEY(idCommand) REFERENCES COMMAND(idCommand),\n"
-                    + "    PRIMARY KEY(idReceipt, idCommand)\n"
-                    + ");";
-            String sql8 = "CREATE TABLE IF NOT EXISTS WAREHOUSE_CATEGORY (\n"
-                    + "    idWCategory integer NOT NULL PRIMARY KEY,\n"
-                    + "    name text NOT NULL\n"
-                    + ");";
-            String sql9 = "CREATE TABLE IF NOT EXISTS WAREHOUSE_PRODUCT (\n"
-                    + "    idWProduct integer NOT NULL,\n"
-                    + "    idWCategory integer NOT NULL,\n"
-                    + "    name text NOT NULL,\n"
-                    + "    amount integer NOT NULL,\n"
-                    + "    minimumAmount integer NOT NULL,\n"
-                    + "    FOREIGN KEY(idWCategory) REFERENCES WAREHOUSE_CATEGORY(idWCategory),\n"
-                    + "    PRIMARY KEY(idWProduct, idWCategory)\n"
-                    + ");";
+         
 
             // Create a the tables
             stmt.execute(cr1);
